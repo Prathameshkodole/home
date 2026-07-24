b99fb350a14665b465d2abb8302f84076f4e8a8b
jaggaJasos <kodoleprathamesh@gmail.com>
2026-07-24 19:16:26 +0530

    Time: 3 ms (86.57%) | Memory: 43.7 MB (10.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,51 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+        Stack<Character> stack = new Stack<>();
 
         int len = s.length();
         if(s.length()<=1)return false;
         for(int i=0;i<len;i++){
-            if(s.charAt(i)=='{' || s.charAt(i)=='}'){
-                if(s.charAt(i)=='{'){
-                    stack.push(s.charAt(i));
-                }else if(s.charAt(i)=='}' && stack.size()>0){
-                    if(stack.peek()=='{'){
-                        stack.pop();
-                    }else{
-                        stack.push(s.charAt(i));
-                    }
-                }else{
-                    stack.push(s.charAt(i));
-                }
-
-            }
-            if(s.charAt(i)=='[' || s.charAt(i)==']'){
-                if(s.charAt(i)=='['){
-                    stack.push(s.charAt(i));
-                }else if(s.charAt(i)==']' && stack.size()>0){
-                   
-                        if(stack.peek()=='['){
-                            stack.pop();
-                        }else{
-                        stack.push(s.charAt(i));
-                        }
-                    
-                }else{
-                    stack.push(s.charAt(i));
-                }
-            }
-            if(s.charAt(i)=='(' || s.charAt(i)==')'){
-                if(s.charAt(i)=='('){
-                    stack.push(s.charAt(i));
-                }else if(s.charAt(i)==')' && stack.size()>0){
-                    if(stack.peek()=='('){ stack.pop();
-                    }else{
-                        stack.push(s.charAt(i));
-                    }
-                   
-                }else{
-                    stack.push(s.charAt(i));
-                }
+            char c = s.charAt(i);
+            if(c == '[' || c=='{' || c=='('){
+                stack.push(s.charAt(i));
+            }else if(c==']' && stack.size()>0 && stack.peek()=='['){
+                stack.pop();
+            }else if(c=='}' && stack.size()>0 && stack.peek()=='{'){
+                stack.pop();
+            }else if(c==')' && stack.size()>0 && stack.peek()=='('){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(i));
             }
         }
         if(stack.size()==0){
